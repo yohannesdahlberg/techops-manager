d8516468239b45c33f843bc7fd6d33e7bc830fcc
Yohannes Dahlberg <yohannes.dahlberg@outlook.com>
2026-08-15 00:12:12 +0200

    Read menu selection from user

--- TechOpsManager.Console/Program.cs
+++ TechOpsManager.Console/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("=== TechOps Manager ===");
+
+Console.WriteLine("Select an option:");
+Console.WriteLine("1. View devices");
+Console.WriteLine("2. Add device");
+Console.WriteLine("3. Remove device");
+Console.WriteLine("4. View device details");
+Console.WriteLine("5. Exit");
+
+string optionChoice = Console.ReadLine();
